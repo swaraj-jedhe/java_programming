2f65f3b2125601f7ba6f26ef26c786c0ca5549b4
Swaraj Jedhe <swarajjedhe26@gmail.com>
2024-01-06 14:39:50 +0530

    String_anagram

--- src/String_anagram/program.java
+++ src/String_anagram/program.java
@@ -1,5 +1,33 @@
 package String_anagram;
 
-public class program {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class program {
+public static void main(String[] args) {
+	
+	Scanner scanner =new Scanner(System.in);
+	System.out.println("plz enter 1st word");
+	String first =scanner.next();
+	System.out.println("plz enter second word");
+	String second =scanner.next();
+	
+	char[]arr1=first.toCharArray();
+	char[]arr2=second.toCharArray();
+	
+	Arrays.sort(arr1);
+	Arrays.sort(arr2);
+	if(Arrays.equals(arr1, arr2)) {
+		System.out.println("string is anagram");
+		
+	}
+	else
+	{
+		System.out.println("string is not anagram");
+		
+	}
+	scanner.close();
+}
+
+	
 }
